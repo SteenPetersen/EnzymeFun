ee7d913ba5e1b72ff2444e38ef183f358216dd88
SteenPetersen <Steen@vejenpetersen.dk>
2018-04-23 09:57:12 +0200

    Added a .gitignore file

---
+++ Assets/_Scripts/FirstAttempt.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAttempt : MonoBehaviour {
+
+    [SerializeField]
+    TextAsset tracks;                   // Reference to the Tracks_ statistics data
+    [SerializeField]
+    TextAsset linksToTracks;            // Reference to the Links_In_Tracks_Statistics data
+
+    [SerializeField]
+    string[] lines;                     // a string array to hold the lines in each parsed script
+
+    Dictionary<int, string[]> valuesInLine = new Dictionary<int, string[]>();
+    Dictionary<int, List<Vector2>> plots = new Dictionary<int, List<Vector2>>();
+
+    char lineSeperator = '\n';          // character with which to seperate by lines
+    char fieldSeperator = ',';          // character with which to seperate by lines
+
+    [SerializeField]
+    GameObject enzyme;                  // the gameobject that will represent the enzymes
+
+    [SerializeField]
+    int numberOfFieldsParsed = 0;
+
+    List<Vector2> tmpVectors = new List<Vector2>(); // temporary list used to fill the Plot dictionary for each enzyme.
+
+    private void Start()
+    {
+        SetUpLines(linksToTracks);
+        SetUpFields(lines);
+        CreatePlotDictionary();
+    }
+
+    private void Update()
+    {
+        SpawnEnzyme();
+    }
+
+    private void SpawnEnzyme()
+    {
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    var enzymeObj = Instantiate(enzyme, null, true);
+        //    var positions = plots[1];
+
+        //    var script = enzymeObj.GetComponent<EnzymeMovement>();
+        //    script.placesToGo = positions;
+        //    enzymeObj.transform.position = positions[0];
+
+        //    foreach (var item in plots[1])
+        //    {
+        //        Debug.Log(item);
+        //    }
+
+        //}
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+
+        }
+
+    }
+
+    private void CreatePlotDictionary()
+    {
+        List<int> keyList = new List<int>(this.valuesInLine.Keys);
+        List<Vector2> vectors = new List<Vector2>();
+
+        var currentEnzyme = 0;
+        var previousEnzyme = 0;
+
+        foreach (var key in keyList)
+        {
+            //Debug.Log(key);
+            var lineOfValues = valuesInLine[key];
+
+
+            int.TryParse(lineOfValues[2], out currentEnzyme);
+            Debug.Log(currentEnzyme);
+
+            if (currentEnzyme == 0)
+                continue;
+
+            if (currentEnzyme == 1)
+            {
+                float enzymeXPos = 0.0000f;
+                float enzymeYPos = 0.0000f;
+
+                float.TryParse(lineOfValues[7], out enzymeXPos);
+                float.TryParse(lineOfValues[8], out enzymeYPos);
+
+                Vector2 tmp = new Vector2(enzymeXPos, enzymeYPos);
+
+                vectors.Add(tmp);
+                Debug.Log(tmp);
+
+                previousEnzyme = currentEnzyme;
+
+                continue;
+            }
+
+            Debug.Log(vectors.Count);
+
+            foreach (var item in vectors)
+            {
+                Debug.Log(item);
+            }
+
+            Debug.Log(previousEnzyme);
+
+            plots.Add(previousEnzyme, vectors);
+
+            Debug.Log("ending");
+            return;
+
+            // Debug.Log(lineOfValues[2]);
+            // go through every single value
+
+        }
+    }
+
+    public void SetUpLines(TextAsset csvFile)
+    {
+        lines = csvFile.text.Split(lineSeperator);
+    }
+
+    void SetUpFields(string[] lines)
+    {
+        foreach (var line in lines)
+        {
+            if (line != "")
+            {
+                string[] fields = line.Split(fieldSeperator);
+
+                this.valuesInLine.Add(numberOfFieldsParsed, fields);
+
+                numberOfFieldsParsed++;
+            }
+
+        }
+    }
+}
---
+++ Assets/_Scripts/ImportData.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ImportData : MonoBehaviour {
+
+    [SerializeField]
+    GameObject pacman;
+
+    [SerializeField]
+    TextAsset links;
+
+    char fieldSeperator = ',';
+    char lineSeperator = '\n';
+
+    [SerializeField]
+    string[] lines;
+
+    Dictionary<int, List<Vector2>> data = new Dictionary<int, List<Vector2>>();
+
+    /// <summary>
+    /// A public property from which outside scripts can fetch the information in in the "data" dictionary
+    /// </summary>
+    public Dictionary<int, List<Vector2>> MyData
+    {
+        get
+        {
+            return data;
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        SetUpLines(links);
+        SetUpFields(lines);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Populates the lines array with every line in the provided csv file
+    /// </summary>
+    /// <param name="csvFile">a .CSV file to be parsed</param>
+    public void SetUpLines(TextAsset csvFile)
+    {
+        lines = csvFile.text.Split(lineSeperator);
+    }
+
+    /// <summary>
+    /// Extracts the fields from each line in the lines array and subsequently places those values in 
+    /// the Data dictionary where the Key is the track number, and it's value is a list of Vector2 structs.
+    /// </summary>
+    /// <param name="lines"></param>
+    void SetUpFields(string[] lines)
+    {
+        // run though all lines
+        foreach (var line in lines)
+        {
+            // if the line is not empty and the line is not the first lines
+            if (line != "" && line != lines[0])
+            {
+                // initialize a list of strings called "valuesInLine" and split the line at every comma to populate it
+                List<string> valuesInLine = line.Split(fieldSeperator).ToList();
+
+                // create a new Vector2 that takes the 7th and 8th value in the valuesInLine list
+                // casts them as floats and set them as the Vector2 values.
+                Vector2 vectorInThisLine = new Vector2(float.Parse(valuesInLine[7]), float.Parse(valuesInLine[8]));
+
+                // if the data dictionary already has the key
+                if (data.ContainsKey(int.Parse(valuesInLine[2])))
+                {
+                    // then simply add the vector2 to the list it already contains
+                    data[int.Parse(valuesInLine[2])].Add(vectorInThisLine);
+                }
+                else
+                {
+                    // otherwise add a new Key with and set its values to be a new list with the 
+                    // current vector2 as its first entry
+                    data.Add(int.Parse(valuesInLine[2]), new List<Vector2>() { vectorInThisLine });
+                }
+            }
+
+        }
+
+        //foreach (KeyValuePair<int, Vector2> kvp in data)
+        //{
+        //    //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+        //    textBox3.Text += string.Format("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+        //}
+    }
+}
